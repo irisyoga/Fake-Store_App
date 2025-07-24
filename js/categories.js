26fddc767284925f39e1cbf6f8d9879e6e539e43
Iryna_Haber <gaber.ira@gmail.com>
2025-07-24 17:55:42 +0200

    Hw18: Categories: Delete

--- js/categories.js
+++ js/categories.js
@@ -3,39 +3,107 @@ const messageEl = document.getElementById("message");
 
 async function fetchCategories() {
   const res = await fetch("https://api.escuelajs.co/api/v1/categories");
+  if (!res.ok) {
+    throw Error("Failed to fetch categories");
+  }
   const categories = await res.json();
+  //const{id, image, name, slug} = obj;
   console.log(categories);
 
   categories.forEach((category) => {
-    const { name, image } = category;
+    const { id, image, name } = category; // мы достали все эти вещи с помощью дист.
 
-    const item = document.createElement("li");
-    item.classList.add("category-card");
+    //Создаем элемент списка
+    const itemCard = document.createElement("li");
+    itemCard.id = "category-" + id; //присваиваем id элементу списка
+    itemCard.classList.add("category-card");
 
+    //Название
     const nameEl = document.createElement("h2");
     nameEl.textContent = name;
 
+    //Изображение
     const img = document.createElement("img");
-    img.classList.add("category-image");
     img.src = image;
+    img.classList.add("category-image");
     img.referrerPolicy = "no-referrer";
 
-    item.appendChild(nameEl);
-    item.appendChild(img);
-    categoryList.appendChild(item);
+    //Форма редактирования (изначально скрыта)
+    const form = document.createElement("form");
+    form.style.display = "none";
+    form.innerHTML = `
+      <input type="text" name="name" placeholder="name" value="${name}">
+      <input type="text" name="image" placeholder="image" value="${image}">
+      <button type="submit">Save</button>`;
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      //console.log(event.target.name.value);
+      //console.log(event.target.image.value);
+      fetchUpdateCategory(
+        id,
+        event.target.name.value,
+        event.target.image.value,
+        itemCard
+      );
+    });
+
+    //Кнопка "Edit"
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.type = "button";
+    editBtn.onclick = () => {
+      if (form.style.display === "block") {
+        form.style.display = "none";
+      } else {
+        form.style.display = "block";
+      }
+    };
+
+    //Кнопка "Delete"
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.type = "button";
+
+    deleteBtn.onclick = () => {
+      fetchDeleteCategory(id, itemCard);
+    };
+
+    //Добавляем все элементы списка
+    itemCard.append(nameEl, img, editBtn, deleteBtn, form);
+    categoryList.append(itemCard);
   });
+}
 
-   if (res.ok) {
-    messageEl.textContent = "Successfully category loading";
-  } else {
-    messageEl.textContent = "Error loading category!";
+async function fetchUpdateCategory(id, name, image, itemCard) {
+  const res = await fetch(`https://api.escuelajs.co/api/v1/categories/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ name, image }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (res.ok) {
+    //item.firstChild.textContent = name;
+    itemCard.querySelector("h2").textContent = name;
+    itemCard.getElementsByTagName("img")[0].src = image;
   }
- 
-} 
+}
+//Удаление категорий
+/*
+async function fetchDeleteCategory(id, itemCard) {
+  const res = await fetch(`https://api.escuelajs.co/api/v1/categories/${id}`, {
+ //  method: "DELETE",
+}); 
+  if (res.ok) {
+    categoryList.removeChild(itemCard);
+    //window.location.href = "/";//информация будет обновляться с перезагрузки страницы в браузере
+    
+  }
+}*/
+function fetchDeleteCategory(id, itemCard) {
+  const element = document.getElementById("category-" + id);
+  if (itemCard) {
+    itemCard.remove();
+  } 
+}  
+
 fetchCategories();
-
-
-
-
-
-
--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 const productsList = document.getElementById("products-list");
 
 // объявим асинхронную функцию
@@ -9,10 +8,10 @@ async function fetchProducts() {
   console.log(products);
 
   products.forEach((product) => {
-    
-    const { title, description, images } = product;
+    const { title, description, images, id, price } = product;
 
     const productCard = document.createElement("li");
+    productCard.id = "product-" + id;
     productCard.classList.add("product-card"); //добавили класс через js, чтобы было удобно стилизовать
 
     const titleEl = document.createElement("h2");
@@ -24,7 +23,52 @@ async function fetchProducts() {
     img.src = images[0];
     img.referrerPolicy = "no-referrer";
 
-    productCard.append(titleEl, img, descriptionEl);
+    const priceElement = document.createElement("p");
+    priceElement.textContent = "Price: " + price;
+
+    //Кнопка удаления 
+    const deleteBtn = document.createElement("button");
+    deleteBtn.type = "button";
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = () => {
+      //можно повесить при клике на этот элемент отработает какая-то функция
+      //console.log(id);
+      fetchDeleteProduct(id);
+    };
+
+    const editBtn = document.createElement("button");
+    editBtn.type = "button";
+    editBtn.textContent = "Edit";
+    editBtn.onclick = () => {
+      const form = document.createElement("form");
+      form.id = "edit-product-" + id;
+
+      form.innerHTML = `
+      <input type="text" name="title" placeholder="title" value="${title}">
+      <input type="text" name="price" placeholder="price" value="${price}">
+      <button type="submit">Save</button>`;
+
+      Card.appendChild(form);
+
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        fetchUpdateProduct(
+          id,
+          event.target.title.value,
+          Number(event.target.price.value)
+        );
+      });
+    };
+
+    productCard.append(
+      titleEl,
+      img,
+      descriptionEl,
+      priceElement,
+      deleteBtn,
+      editBtn
+    );
     productsList.appendChild(productCard);
   });
 }
@@ -33,3 +77,30 @@ async function fetchProducts() {
 fetchProducts();
 
 // CRUD = Create Read Update Delete
+
+//Удаление продукта
+async function fetchDeleteProduct(productId) {
+  const productCard = document.getElementById("product-" + productId);
+  const res = await fetch(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    { method: "DELETE" }
+  );
+
+  if (res.ok) {
+    productsList.removeChild(productCard);
+    //window.location.href = "/";//информация будет обновляться с перезагрузки страницы в браузере
+  }
+}
+
+async function fetchUpdateProduct(id, title, price) {
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ title, price }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!res.ok) {
+    console.error("Category update error:", await res.json());
+  }
+}
+fetchCategories();
